92ae058b7a264a9bda139a5be6f7e5d6f9f63b02
“carlosmolano” <carlosantoniomolanosolarte@gmail.com>
2018-10-20 09:55:01 -0500

    routes 😺

--- facepets/src/app/app.module.ts
+++ facepets/src/app/app.module.ts
@@ -3,16 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PetsComponent } from './pets/pets.component';
+import {RouterModule, Routes} from "@angular/router";
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProfileComponent } from './profile/profile.component';
+import { InicioComponent } from './inicio/inicio.component';
+
+const rutas: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'inicio', component: InicioComponent},
+  {path: 'login',component: LoginComponent},
+  {path: 'dashboard',component: DashboardComponent},
+  {path: 'perfil', component: ProfileComponent},
+  {path: 'pet',component: PetsComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    PetsComponent
+    PetsComponent,
+    LoginComponent,
+    DashboardComponent,
+    ProfileComponent,
+    InicioComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(rutas)
   ],
   providers: [],
-  bootstrap: [AppComponent, PetsComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
