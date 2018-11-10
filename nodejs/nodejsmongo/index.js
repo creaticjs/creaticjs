32002045170f4e47e5689927bf9723800fabf018
“carlosmolano” <carlosantoniomolanosolarte@gmail.com>
2018-11-09 21:11:36 -0500

    Nodejs Mongoose 😺

---
+++ nodejs/nodejsmongo/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongo = require('./db/dbconnection');
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+const routePets = require('./routes/pets')(app);
+
+
+mongo.conectar(app);
